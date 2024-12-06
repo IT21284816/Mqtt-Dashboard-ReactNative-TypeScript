c39de96b9479d2b56088f9242a81a7e510ec4eeb
Duhun De Silva <sandaruduhun@gmail.com>
2024-12-06 18:00:16 +0530

    build(deps): update App.tsx, package-lock.json and package.json

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native'; // Import StatusBar
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './components/home';
@@ -9,6 +10,8 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
+      {/* Make sure StatusBar is visible and configured */}
+      <StatusBar barStyle="light-content" />
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Settings" component={Settings} />
